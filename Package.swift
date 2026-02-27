fe6e894035c8f9994056718b05df97d64335ca05
Shmakov Igor <shmakoff.work@gmail.com>
2026-02-27 11:33:13 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Realm",
-      url: "https://github.com/shmakovigor/realm/releases/download/v10.54.6/Realm.xcframework.zip",
-      checksum: "6bebe800f5fe04e4dd3a0566670fbaa1986d68262652070d9810f46dc33258bd"
+      url: "https://github.com/shmakovigor/realm/releases/download/v20.0.4/Realm.xcframework.zip",
+      checksum: "0014a034372f46e0cdbd6145f83f698e932e0866a0090ed34d60ab4ff4810e91"
     ),
     .binaryTarget(
       name: "RealmSwift",
-      url: "https://github.com/shmakovigor/realm/releases/download/v10.54.6/RealmSwift.xcframework.zip",
-      checksum: "7c21c29c023224b60ddd303fbe7930bbbfe8acc3f9db2532430cf9fee1cf30ac"
+      url: "https://github.com/shmakovigor/realm/releases/download/v20.0.4/RealmSwift.xcframework.zip",
+      checksum: "b39fa5be0d2bbf97d360ce4aa088bee8bd6d99189d4974fc2ba36274e1c888e3"
     ),
   ]
 )
