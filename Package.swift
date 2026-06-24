f47e3722e3ab4053bfb8e0215ef314d5aa5584df
Shmakov Igor <shmakoff.work@gmail.com>
2026-06-24 07:03:01 +0100

    Update Realm and RealmSwift checksums in Package.swift

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     .binaryTarget(
       name: "Realm",
       url: "https://github.com/realm/realm-swift/releases/download/v20.0.5/Realm.spm.zip",
-      checksum: "2c6c760730b3cca3eee93bbe138df696f85ed46550222f310976be226bc693e8"
+      checksum: "3598552815548c4ef583e53ee84ffc3c8d2233fd0eedad1303f59f6ef5b798cc"
     ),
     .binaryTarget(
       name: "RealmSwift",
       url: "https://github.com/realm/realm-swift/releases/download/v20.0.5/RealmSwift@26.5.spm.zip",
-      checksum: "3598552815548c4ef583e53ee84ffc3c8d2233fd0eedad1303f59f6ef5b798cc"
+      checksum: "cfc9f0e708287cf8805bd01eddcdbd4cfe0a6cba9413ea391a99a77b4e913d93"
     ),
   ]
 )
